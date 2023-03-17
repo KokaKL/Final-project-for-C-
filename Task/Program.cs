950b441d5069ff54767e84dd30efa36f0ee284e5
Koka <kshurgaya91@gmail.com>
2023-03-17 15:27:40 +0300

    функции

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Написать программу, которая из имеющегося массива строк 
 // формирует массив из строк, длина которых <= 3.
 
+string PromtString(string? msg)
+{
+    System.Console.Write(msg + "-> ");
+    return Console.ReadLine();
+}
+
+int PromtInt(string? msg)
+{
+    System.Console.Write(msg + "-> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + "; ");
+    }
+}
